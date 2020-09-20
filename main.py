e81ffb951dc989dd076f4d877082d555730f2af2
VeerSandhu <sandhu.s.veer@gmail.com>
2020-09-20 12:32:42 -0400

    tested with new digits

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-from keras.models import load_model
 import tensorflow as tf
+import numpy as np
+from PIL import Image
 
-model = load_model("models\seq_model1.h5")
-prob_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
+base_model = tf.keras.models.load_model('models/seq_model2.h5')
+prob_model = tf.keras.Sequential([base_model, tf.keras.layers.Softmax()])
+
+img1 = Image.open("new digits/7.png").convert(mode="F")
+array1 = np.array(img1.getdata())
+
+x = array1.reshape(28,28)
+x = np.expand_dims(x, axis=0) / 255.0
+
+prob_result = prob_model.predict(x)
+
+print(prob_result)
+print(np.argmax(prob_result))
